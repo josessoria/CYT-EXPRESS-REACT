1f32218c29d8e8419d420389dbeff9d60b1d1193
jose <josesoriap21@gmail.com>
2024-09-13 13:06:48 -0300

    searchbar

--- CytFrontend/src/components/Products/Products.tsx
+++ CytFrontend/src/components/Products/Products.tsx
@@ -5,6 +5,8 @@ import { useProductContext } from "../../context/ProductContext";
 import Logo from "../../assets/image/cytlogo.png";
 import { FaTrash } from "react-icons/fa";
 import { UserContext, UserContextType } from "../../context/UserProvider";
+import { Input } from "@nextui-org/react";
+import { CiSearch } from "react-icons/ci";
 
 interface Product {
   _id: string;
@@ -23,19 +25,19 @@ const Products: React.FC = () => {
   const [groupedProducts, setGroupedProducts] = useState<{
     [key: string]: Product[];
   }>({});
-
+  const [searchTerm, setSearchTerm] = useState<string>(""); // Estado para la búsqueda
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        setLoading(true); // Start loading
+        setLoading(true);
         const response = await axios.get<Product[]>("/api/Products");
         setProducts(response.data);
       } catch (error) {
         console.error("Error al obtener los productos:", error);
       } finally {
-        setLoading(false); // End loading
+        setLoading(false);
       }
     };
 
@@ -50,6 +52,7 @@ const Products: React.FC = () => {
       console.error("Error al eliminar el producto", error);
     }
   };
+
   useEffect(() => {
     const grouped = products.reduce((acc, product) => {
       const category = product.category?.name || "Uncategorized";
@@ -62,20 +65,43 @@ const Products: React.FC = () => {
 
     setGroupedProducts(grouped);
   }, [products]);
-  const noProducts = Object.keys(groupedProducts).length === 0;
+
+  // Filtrar productos en base al término de búsqueda
+  const filteredProducts = Object.keys(groupedProducts).reduce(
+    (acc, category) => {
+      const filtered = groupedProducts[category].filter((product) =>
+        product.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      if (filtered.length > 0) {
+        acc[category] = filtered;
+      }
+      return acc;
+    },
+    {} as { [key: string]: Product[] }
+  );
+
+  const noProducts = Object.keys(filteredProducts).length === 0;
+
   return (
     <div className="w-full p-4">
+      <div className="input w-full flex justify-center items-center">
+        <Input
+          type="text"
+          placeholder="Buscar producto..."
+          className="mb-4  max-w-[600px] rounded w-full"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          startContent={
+            <CiSearch className="text-black/50 mb-0.5  text-[20px] dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+          }
+        />
+      </div>
+
       {loading && (
         <div>
-          <div className="mb-8">
-            <h2 className="text-2xl font-bold mb-4">Loading...</h2>
-            <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-              <div className="loader" />
-              <div className="loader" />
-              <div className="loader" />
-              <div className="loader" />
-              <div className="loader" />
-            </div>
+          <h2 className="text-2xl font-bold mb-4">Loading...</h2>
+          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            <div className="loader" />
           </div>
         </div>
       )}
@@ -86,14 +112,13 @@ const Products: React.FC = () => {
         </div>
       )}
 
-      {/* Principal Content */}
       {!loading &&
         !noProducts &&
-        Object.keys(groupedProducts).map((category) => (
+        Object.keys(filteredProducts).map((category) => (
           <div key={category} className="mb-8">
             <h2 className="text-2xl font-bold mb-4">{category}</h2>
             <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-              {groupedProducts[category].map((product) => (
+              {filteredProducts[category].map((product) => (
                 <div
                   key={product._id}
                   className="bg-white flex flex-col p-2 rounded-2xl shadow-lg"
@@ -113,18 +138,18 @@ const Products: React.FC = () => {
                     <h3 className="text-xl text-start font-medium  text-[25px] mt-[10px]">
                       {product.name}
                     </h3>
-                    <p className="font-[700] leading-none   text-[#804ffa] text-start text-[30px]">
+                    <p className="font-[700] leading-none text-[#804ffa] text-start text-[30px]">
                       {product.price.toFixed(2)}$
                     </p>
                     <p className="text-gray-600 text-justify text-[15px]">
                       {product.description}
                     </p>
                     <div className="addtocart w-full gap-4 flex">
-                      <div className=" w-1/2 h-[40px] flex text-[#804ffa] justify-center  rounded-2xl cursor-pointer items-center border border-[#804ffa]  ">
+                      <div className=" w-1/2 h-[40px] flex text-[#804ffa] justify-center rounded-2xl cursor-pointer items-center border border-[#804ffa]">
                         Comprar
                       </div>
-                      <div className=" w-1/2 h-[40px] flex text-white justify-center  rounded-2xl cursor-pointer items-center bg-[#804ffa] ">
-                       Añadir a carrito
+                      <div className=" w-1/2 h-[40px] flex text-white justify-center rounded-2xl cursor-pointer items-center bg-[#804ffa]">
+                        Añadir a carrito
                       </div>
                     </div>
                   </div>
